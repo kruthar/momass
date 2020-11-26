89f4638fc010a18e97effe45e2ddbae1bed494b5
Andy Kruth <kruthar@gmail.com>
2020-11-26 11:08:59 -0500

    all current assets

---
+++ Assets/Scripts/Draggable.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+  private bool dragging = false;
+  private bool inShelves = false;
+  private float distance;
+  private float dragZ = -1.0f;
+  private Vector3 home;
+
+  public GameObject shelves;
+
+  void Start() {
+    home = transform.position;
+  }
+
+  public void Reset() {
+    transform.position = home;
+    transform.rotation = Quaternion.identity;
+  }
+
+  void OnMouseDown()
+  {
+    Vector3 newPos = transform.position;
+    newPos.z = dragZ;
+    transform.position = newPos;
+
+      distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+      dragging = true;
+  }
+
+  void OnTriggerEnter(Collider c) {
+    Debug.Log("collide: " + c.gameObject);
+    if (c.gameObject.name == "Shelves") {
+      inShelves = true;
+    }
+  }
+
+  void OnTriggerExit(Collider c) {
+    if (c.gameObject.name == "Shelves") {
+      inShelves = false;
+    }
+  }
+
+  void OnMouseUp()
+  {
+      dragging = false;
+      if (inShelves) {
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+        transform.position = home;
+      }
+  }
+
+  public bool isDragging() {
+    return dragging;
+  }
+
+  void Update()
+  {
+      if (dragging)
+      {
+          Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+          Vector3 rayPoint = ray.GetPoint(distance);
+          Vector3 newPos = new Vector3(rayPoint.x, rayPoint.y, dragZ);
+          transform.position = newPos;
+          transform.rotation = Quaternion.identity;
+      }
+  }
+}
---
+++ Assets/Scripts/ScaleController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScaleController : MonoBehaviour
+{
+    public TextMeshProUGUI displayText;
+    private List<Collider> colliders = new List<Collider>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void Reset() {
+      foreach (GameObject flask in GameObject.FindGameObjectsWithTag("Flask")) {
+        flask.GetComponent<Draggable>().Reset();
+      }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        double sum = 0.0;
+        foreach (Collider c in colliders) {
+          if (!c.gameObject.GetComponent<Draggable>().isDragging()) {
+            sum += c.gameObject.GetComponent<Properties>().mass;
+          }
+        }
+        displayText.text = string.Format("{0:0.00}", sum);
+    }
+
+    private void OnTriggerEnter (Collider other) {
+      if (!colliders.Contains(other)) {
+        colliders.Add(other);
+        Debug.Log("inside: " + other);
+      }
+    }
+
+    private void OnTriggerExit (Collider other) {
+      colliders.Remove(other);
+      Debug.Log("outside: " + other);
+    }
+}
